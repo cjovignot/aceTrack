b10db03d78fb255ed8e5fef601eb268898c202a8
cjovignot <cosme.jovignot@epitech.eu>
2026-04-17 13:51:46 +0200

    [PWA]

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,18 @@ import { AuthProvider } from "../context/AuthContext";
 
 export const metadata = {
   title: "aceTrack",
-  description: "Starter Kit NextJS 15 : Start Now !",
+  description: "Score en direct",
+
+  // ✅ PWA
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+
+  // ✅ iOS support
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "aceTrack",
+  },
 };
 
 export default function RootLayout({ children }) {
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // pas de PWA en dev
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
