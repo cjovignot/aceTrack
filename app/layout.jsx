89ada67b7bd5609d40c2a183f285a70657848cd6
cjovignot <cosme.jovignot@epitech.eu>
2026-04-17 17:10:04 +0200

    [PWA]

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
+    <html lang="fr" <link rel="manifest" href="/manifest.json">>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
