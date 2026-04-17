816bea1250905322afe724ea252eb149ddcb696e
cjovignot <cosme.jovignot@epitech.eu>
2026-04-17 17:15:29 +0200

    [PWA]

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,9 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr"
-    <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" href="/icons/icon-180.png">
     >
+        <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/icons/icon-180.png">
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
