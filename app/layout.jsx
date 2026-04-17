009e8dfaf89f3c9ea1a8101a0de2706bc26f3406
cjovignot <cosme.jovignot@epitech.eu>
2026-04-17 17:17:54 +0200

    [PWA]

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,24 +5,23 @@ export const metadata = {
   title: "aceTrack",
   description: "Score en direct",
 
-  // ✅ PWA
   manifest: "/manifest.json",
   themeColor: "#000000",
 
-  // ✅ iOS support
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
     title: "aceTrack",
   },
+
+  icons: {
+    apple: "/icons/icon-180.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr"
-    >
-        <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" href="/icons/icon-180.png">
+    <html lang="fr">
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
